bcb758f22d42085cf8ba70704c90a71e5b5b3cbe
nttoan1995 <thanhtoan12th@gmail.com>
2016-06-08 14:30:15 +0700

    qwe
    eqwe

---
+++ FinalCrypt/_danhsachnguoidung.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FinalCrypt
+{
+    class _danhsachnguoidung
+    {
+        private List<_nguoidung> dsnguoidung = new List<_nguoidung>();
+        public _danhsachnguoidung(StreamReader fs)
+        {
+            while( !fs.EndOfStream)
+            {
+                _nguoidung nguoidunghientai = new _nguoidung();
+                nguoidunghientai.docthongtin(fs);
+                dsnguoidung.Add(nguoidunghientai);
+            }
+        }
+
+    }
+}
